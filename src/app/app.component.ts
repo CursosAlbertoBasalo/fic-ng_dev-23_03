6905d6516fec602936e25c10b56b2d0ae322fe99
Alberto Basalo <albertobasalo@hotmail.com>
2023-03-16 17:16:59 +0100

    feat: layout module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 
+// templateUrl: './app.component.html',
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <app-header></app-header>
+    <main>Main content</main>
+    <app-footer></app-footer>
+  `,
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'CITIC FIC UDC ';
-  public cantidad: number = 0;
-  precio = 0;
-}
+export class AppComponent {}
