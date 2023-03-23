5b20292f44ae39b26a4f456a1655edbe507e4129
Alberto Basalo <albertobasalo@hotmail.com>
2023-03-23 18:21:08 +0100

    feat: event order

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,7 +9,19 @@ import { ACTIVITIES } from '../data/activities.data';
 export class HomeComponent {
   //private activities: Activity[] = ACTIVITIES;
 
-  publishedActivities = ACTIVITIES.filter((a) => a.state === 'published');
+  order = 1;
+
+  publishedActivities = ACTIVITIES.filter((a) => a.state === 'published').sort(
+    (a, b) => (a.price - b.price) * this.order
+  );
+
+  changeOrder() {
+    this.order = this.order * -1;
+    console.log(this.order);
+    this.publishedActivities = ACTIVITIES.filter(
+      (a) => a.state === 'published'
+    ).sort((a, b) => (a.price - b.price) * this.order);
+  }
 
   // getPublishedActivities(): Activity[] {
   //   return this.activities.filter((a) => a.state === 'published');
