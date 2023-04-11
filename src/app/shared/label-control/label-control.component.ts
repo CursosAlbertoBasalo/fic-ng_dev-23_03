ec0a51e76e6ad593804ad96af7b978b27762e6bf
Alberto Basalo <albertobasalo@hotmail.com>
2023-04-11 17:19:59 +0200

    feat: refactor forms

--- src/app/shared/label-control/label-control.component.ts
+++ src/app/shared/label-control/label-control.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-label-control',
   templateUrl: './label-control.component.html',
-  styleUrls: ['./label-control.component.css']
+  styleUrls: ['./label-control.component.css'],
 })
 export class LabelControlComponent {
-
+  @Input() caption: string = '';
+  @Input() controlName: string = '';
+  @Input() showError: boolean = false;
+  @Input() errorMessage: string = '';
+  @Input() isRequired: boolean = false;
 }
