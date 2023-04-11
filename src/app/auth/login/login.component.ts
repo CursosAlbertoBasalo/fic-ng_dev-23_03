61d86d2bde2f3997c4eb8212d8b29efc8c0d708d
Alberto Basalo <albertobasalo@hotmail.com>
2023-04-11 16:15:08 +0200

    feat: login module

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  form: FormGroup;
+  constructor(formBuilder: FormBuilder) {
+    this.form = formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: [
+        '',
+        [Validators.required, Validators.minLength(4), Validators.maxLength(8)],
+      ],
+    });
+  }
+  showError(controlName: string): boolean {
+    const control = this.form.controls[controlName];
+    if (!control) return false;
+    return control.invalid && (control.touched || control.dirty);
+  }
 
+  getError(controlName: string): string {
+    const control = this.form.controls[controlName];
+    if (!control) return '';
+    return JSON.stringify(control.errors);
+  }
+
+  onLoginClick() {
+    console.log('Login button clicked');
+    console.log('Form', this.form.value);
+  }
 }
--- src/app/auth/login/login.module.ts
+++ src/app/auth/login/login.module.ts
@@ -1,17 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { SharedModule } from 'src/app/shared/shared.module';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 
-
 @NgModule({
-  declarations: [
-    LoginComponent
-  ],
-  imports: [
-    CommonModule,
-    LoginRoutingModule
-  ]
+  declarations: [LoginComponent],
+  imports: [CommonModule, LoginRoutingModule, SharedModule],
 })
-export class LoginModule { }
+export class LoginModule {}
